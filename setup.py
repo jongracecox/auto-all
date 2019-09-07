2eeab3a8466edd8a2d43bc960e591f22fb39b62e
Jon Grace-Cox <jongracecox@gmail.com>
2019-09-07 15:50:37 -0400

    Include readme in PyPi long description

--- setup.py
+++ setup.py
@@ -2,6 +2,20 @@
 import os
 import re
 from setuptools import setup
+from m2r import parse_from_file
+import restructuredtext_lint
+
+# Parser README.md into reStructuredText format
+rst_readme = parse_from_file('README.md')
+
+# Validate the README, checking for errors
+errors = restructuredtext_lint.lint(rst_readme)
+
+# Raise an exception for any errors found
+if errors:
+    print(rst_readme)
+    raise ValueError('README.md contains errors: ',
+                     ', '.join([e.message for e in errors]))
 
 # Attempt to get version number from TravisCI environment variable
 version = os.environ.get('TRAVIS_TAG', default='0.0.0')
@@ -12,6 +26,7 @@ version = re.sub('^v', '', version)
 setup(
     name='auto-all',
     description='Automatically manage __all__ variable in Python packages.',
+    long_description=rst_readme,
     version=version,
     author='Jon Grace-Cox',
     author_email='jongracecox@gmail.com',
